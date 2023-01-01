9c399f95aa19acd45d9c7850d8b3d8ffa7464761
marcoantn020 <marcoantn020@gmail.com>
2022-12-31 21:16:00 -0300

    feat: opcoes do menu de forma dinamica dinamico

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,15 +1,43 @@
 import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material'
 import { Box } from '@mui/system'
+import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom'
 import { useDrawerContext } from '../../contexts'
 
-interface IMenuLateralProps {
-  children: React.ReactNode
+
+interface IListItemLinkProps {
+  to: string
+  icon: string
+  label: string
+  onClick: (() => void) | undefined
 }
+const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }) => {
+  const navigate = useNavigate()
+
+  const resolvedPath = useResolvedPath(to)
+  const match = useMatch({ path: resolvedPath.pathname, end: false})
+
+  const handleClick = () => {
+    navigate(to)
+    onClick?.()
+  }
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={label} />
+    </ListItemButton>
+  )
+}
+
+
+interface IMenuLateralProps {children: React.ReactNode}
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
   const theme = useTheme()
   const smDown = useMediaQuery(theme.breakpoints.down('sm'))
 
-  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext()
+  const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext()
     
   return (
     <>
@@ -31,12 +59,15 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
 
           <Box flex={1}>
             <List component="nav">
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>home</Icon>
-                </ListItemIcon>
-                <ListItemText primary="Pagina inicial" />
-              </ListItemButton>
+              {drawerOptions.map(drawerOption => (
+                <ListItemLink
+                  key={drawerOption.path}
+                  icon={drawerOption.icon}
+                  label={drawerOption.label}
+                  to={drawerOption.path}
+                  onClick={smDown ? toggleDrawerOpen : undefined}
+                />
+              ))}
             </List>
           </Box>
             
--- src/shared/contexts/DrawerContext.tsx
+++ src/shared/contexts/DrawerContext.tsx
@@ -1,29 +1,38 @@
 import { createContext, useCallback, useContext, useState } from 'react'
 
+interface IDrawerOptions {
+  icon: string
+  path: string
+  label: string
+}
 
 interface IDrawerContextData {
     isDrawerOpen: boolean
+    drawerOptions: IDrawerOptions[]
     toggleDrawerOpen: () => void
+    setDrawerOptions: (newDrawerOptions: IDrawerOptions[]) => void
 }
-
 const DrawerContext = createContext({} as IDrawerContextData)
 
 export const useDrawerContext = () => {
   return useContext(DrawerContext)
 }
 
-interface IDrawerProviderProps {
-    children: React.ReactNode
-}
+interface IDrawerProviderProps {children: React.ReactNode}
 export const DrawerProvider: React.FC<IDrawerProviderProps>  = ({ children }) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
+  const [drawerOptions, setDrawerOptions] = useState<IDrawerOptions[]>([])
 
   const toggleDrawerOpen = useCallback(() => {
     setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen)
   }, [])
 
+  const handleDrawerOptions = useCallback((newDrawerOptions: IDrawerOptions[]) => {
+    setDrawerOptions(newDrawerOptions)
+  }, [])
+
   return (
-    <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen }}>
+    <DrawerContext.Provider value={{ isDrawerOpen, drawerOptions, toggleDrawerOpen, setDrawerOptions: handleDrawerOptions }}>
       {children}
     </DrawerContext.Provider>
   )
