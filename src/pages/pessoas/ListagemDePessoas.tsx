268ed548cdab230148e792b743efb7463924c627
marcoantn020 <marcoantn020@gmail.com>
2023-01-01 18:22:06 -0300

    fea: listando pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,31 +1,43 @@
-import {  useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+
 import { FerramentasDeListagem } from '../../shared/components'
 import { useDebouce } from '../../shared/hooks'
 import { LayoutBaseDePagina } from '../../shared/layouts'
-import { PessoasService } from '../../shared/services/api/pessoas/PessoasService'
+import { IListagemPessoa, PessoasService } from '../../shared/services/api/pessoas/PessoasService'
 
 
 interface IListagemDePessoasProps {children?: React.ReactNode}
 export const ListagemDePessoas: React.FC<IListagemDePessoasProps> = () => {
   const [searchParams, setSearchParams] = useSearchParams()
-  const debouce = useDebouce(3000)
+  const debouce = useDebouce()
+
+  const [rows, setRows] = useState<IListagemPessoa[]>([])
+  const [totalCount, setTotalCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''
   }, [searchParams])
 
   useEffect(() => {
+    setIsLoading(true)
 
     debouce.debounce(() => { 
       
-      PessoasService.getAll(1, busca).then((result) => {
-        if(result instanceof Error) {
-          alert(result.message)
-        } else {
-          console.log(result)
-        }
-      })
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          setIsLoading(false)
+
+          if(result instanceof Error) {
+            alert(result.message)
+          } else {
+            console.log(result)
+            setRows(result.data)
+            setTotalCount(result.totalCount)
+          }
+        })
     })
 
   }, [busca])
@@ -43,7 +55,30 @@ export const ListagemDePessoas: React.FC<IListagemDePessoasProps> = () => {
         />
       )}
     > 
-      Testando
+
+      <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome completo</TableCell>
+              <TableCell>E-mail</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+
+            {rows?.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>Ações</TableCell>
+                <TableCell> {row.nomeCompleto} </TableCell>
+                <TableCell> {row.email} </TableCell>
+              </TableRow>
+            ))}
+
+          </TableBody>
+        </Table>
+      </TableContainer>
+
     </LayoutBaseDePagina>
   )
 }
